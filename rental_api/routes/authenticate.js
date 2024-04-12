0307508f3966dbfd770b0a1064c44c7daa3ba106
CodeMHT <gwalamhlaba@icloud.com>
2024-04-12 15:15:38 +0200

    Addition of port and ssl to db connection

--- rental_api/routes/authenticate.js
+++ rental_api/routes/authenticate.js
@@ -11,8 +11,9 @@ const connect = mysql2.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL
 });
-
 
 
 route.get("/:email/:password", (req, res) => {
--- rental_api/routes/vehicles.js
+++ rental_api/routes/vehicles.js
@@ -12,6 +12,8 @@ const connect = mysql2.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL
 });
 
 //storage for images
