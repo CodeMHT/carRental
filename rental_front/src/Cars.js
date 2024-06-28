e98fd8a4e899fdf5e11170af232e310a71f9ebc0
CodeMHT <gwalamhlaba@icloud.com>
2024-06-28 08:01:23 +0200

    Addition of web.config file commit

--- rental_front/src/Cars.js
+++ rental_front/src/Cars.js
@@ -185,11 +185,6 @@ const Cars = () => {
             </div>
             {/* Footer End */}
 
-
-            {/* Back to Top */}
-            <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
-
-
         </>
     )
 }
